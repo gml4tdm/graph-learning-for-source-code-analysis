e0425310481d5bb97e0dc410002bd577179b401a
Jesse Maarleveld <j.maarleveld@rug.nl>
2024-09-24 11:59:27 +0200

    Add round 1 re-tagging

--- study-selection/initial/agreement.py
+++ study-selection/initial/agreement.py
@@ -17,20 +17,30 @@ def agreement_round_0():
 
 
 def agreement_round_1():
-    AUTHOR_A_COLUMN = 'Include/Exclude'
-    AUTHOR_B_COLUMN = 'Author 2'
+    INCLUDE_AUTHOR_A_RE_TAG = True
+    INCLUDE_AUTHOR_B_RE_TAG = True
+    AUTHOR_A_MAIN_COLUMN = 'Include/Exclude'
+    AUTHOR_A_RE_TAG_COLUMN = 'Author 1 Re-tag'
+    AUTHOR_B_MAIN_COLUMN = 'Author 2'
+    AUTHOR_B_RE_TAG_COLUMN = 'Author 2 Re-tag'
 
     df = pl.read_csv('round1.csv')
     author_a = []
     author_b = []
     for row in df.rows(named=True):
-        if not row[AUTHOR_B_COLUMN]:
+        if not row[AUTHOR_B_MAIN_COLUMN]:
             continue    # Paper not double-checked
-        author_a.append(row[AUTHOR_A_COLUMN].lower() == 'include')
-        author_b.append(row[AUTHOR_B_COLUMN].lower() == 'include')
+        if INCLUDE_AUTHOR_A_RE_TAG and row[AUTHOR_A_RE_TAG_COLUMN]:
+            author_a.append(row[AUTHOR_A_RE_TAG_COLUMN].lower() == 'include')
+        else:
+            author_a.append(row[AUTHOR_A_MAIN_COLUMN].lower() == 'include')
+        if INCLUDE_AUTHOR_B_RE_TAG and row[AUTHOR_B_RE_TAG_COLUMN]:
+            author_b.append(row[AUTHOR_B_RE_TAG_COLUMN].lower() == 'include')
+        else:
+            author_b.append(row[AUTHOR_B_MAIN_COLUMN].lower() == 'include')
+
     print('Agreement:', accuracy_score(author_a, author_b))
     print('Kappa:', cohen_kappa_score(author_a, author_b))
-
 
 def agreement_round_2():
     INCLUDE_AUTHOR_A_RE_TAG = True
